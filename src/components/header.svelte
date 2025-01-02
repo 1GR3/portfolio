2d4100fc82cbff75071896eee9d5d10ebb0deff3
Ivo Gregurec <ivo@toptal.com>
2025-01-02 14:09:32 +0100

    header component

---
+++ src/components/header.svelte
@@ -0,0 +1,26 @@
+<script>
+	// import '../js/canvas.js';
+</script>
+
+<header class="container-fluid px-0">
+	<div class="container pt-5">
+		<div class="row">
+			<div class="col">
+				<p class="lead">Hi, my name is</p>
+				<h1 class="display-1">Ivo<br /><small>Gregurec</small></h1>
+				<p class="fs-5 mb-0">and I'm producing interfaces.</p>
+			</div>
+			<div class="col-12 col-md-auto d-flex align-items-end">
+				<a href="/Ivo_Gregurec-CV.pdf" class="btn btn-primary mt-3 w-100 w-md-auto">Download CV</a>
+			</div>
+			<div class="col-12 col-md-auto d-flex align-items-end">
+				<a href="/IvoGregurec.vcf" class="btn btn-secondary mt-3 w-100 w-md-auto">Add to contacts</a
+				>
+			</div>
+		</div>
+	</div>
+</header>
+<canvas id="canvas" class="w-100" />
+
+<style lang="scss">
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
+<script>
+	import Header from '../components/header.svelte';
+</script>
 
+<Header />
+
+<h1>Hi</h1>
+<p>testing</p>
