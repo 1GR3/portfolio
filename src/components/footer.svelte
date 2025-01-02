ef28fd160c4cab8a0225041232b3c3545d6e454a
Ivo Gregurec <ivo@toptal.com>
2025-01-02 21:21:36 +0100

    contacts footer

---
+++ src/components/footer.svelte
@@ -0,0 +1,73 @@
+<script></script>
+
+<footer id="connect" class="container-fluid mt-5">
+	<div class="row">
+		<div class="col-12 col-md-3 px-0">
+			<a
+				target="blank"
+				class="btn py-4 w-100"
+				style="background-color: rgb(10, 102, 194)"
+				href="https://www.linkedin.com/in/ivo-gregurec-07667947/"
+			>
+				<i class="bi bi-linkedin" /> linkedin</a
+			>
+		</div>
+		<div class="col-12 col-md-3 px-0">
+			<a
+				target="blank"
+				class="btn py-4 w-100"
+				style="background-color: #0f256e"
+				href="https://www.toptal.com/resume/ivo-gregurec1"
+			>
+				<svg
+					viewBox="0 0 30 30"
+					class="s9l9qg-jss93 s9l9qg-jss86 s9l9qg-jss90 s9l9qg-jss26"
+					style="width: 16px; height: 16px;"
+					><path
+						d="m8.065 0 6.664 6.664c.048.043.097.092.15.145l5.814 5.816-9.455 9.405 4.331 4.332-2.898 2.88-6.621-6.621c-.076-.07-.156-.15-.237-.231L0 16.58l9.422-9.368L5.13 2.918 8.065 0Zm4.218 10.442a.425.425 0 0 0-.259 0c-.086.022-.161.065-.312.215L6.33 16.016c-.156.156-.194.225-.22.311a.422.422 0 0 0 0 .258c.026.086.07.162.22.312l1.708 1.71c.156.155.226.193.312.22a.425.425 0 0 0 .259 0c.086-.027.161-.065.307-.215l5.382-5.359c.156-.156.194-.225.22-.311a.422.422 0 0 0 0-.258c-.02-.086-.064-.162-.215-.312l-1.708-1.71c-.156-.155-.226-.193-.312-.22Z"
+						fill="white"
+					/></svg
+				>
+				Toptal</a
+			>
+		</div>
+		<div class="col-12 col-md-3 px-0">
+			<a
+				target="blank"
+				class="btn py-4 w-100"
+				style="background-color: rgb(30, 31, 38)"
+				href="https://codepen.io/1GR3"
+			>
+				<svg
+					viewBox="0 0 65 65"
+					class="s9l9qg-jss93 s9l9qg-jss86 s9l9qg-jss90 s9l9qg-jss26"
+					style="width: 16px; height: 16px;"
+					><path
+						xmlns="http://www.w3.org/2000/svg"
+						d="M3.06 41.732L32 60.932l28.94-19.2V22.268L32 3.068l-28.94 19.2zm57.878 0L32 22.268 3.06 41.732m0-19.463L32 41.47l28.94-19.2M32 3.068v19.2m0 19.463v19.2"
+						stroke="white"
+						stroke-width="6.05"
+					/></svg
+				>
+				Codepen</a
+			>
+		</div>
+		<div class="col-12 col-md-3 px-0">
+			<a
+				target="blank"
+				class="btn py-4 w-100"
+				style="background-color: rgb(36, 41, 47)"
+				href="https://github.com/1GR3/"
+			>
+				<i class="bi bi-github" /> Github</a
+			>
+		</div>
+	</div>
+</footer>
+
+<style lang="scss">
+	#connect .btn {
+		color: white !important;
+		border-radius: 0 !important;
+	}
+</style>
