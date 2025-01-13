e6da914933573c2e272471e41191baa13e71d173
Ivo Gregurec <ivo@toptal.com>
2025-01-13 21:29:44 +0100

    page, include portfolio items

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,43 @@
 <script>
 	import Header from '../components/header.svelte';
 	import Footer from '../components/footer.svelte';
+
+	import Hostelhop from '../portfolio/items/hostelhop.svelte';
+	import Redocher from '../portfolio/items/redocher.svelte';
+	import Vava from '../portfolio/items/vava.svelte';
+	import Spectrackular from '../portfolio/items/spectrackular.svelte';
+	import Kyba from '../portfolio/items/kyba.svelte';
+	import Old from '../portfolio/items/old.svelte';
 </script>
 
 <Header />
 
-<h1>Hi</h1>
-<p>testing</p>
+<div class="container my-5 content-wrapper">
+	<p class="lead">
+		The vast majority of my work, from
+		<span class="badge text-bg-light">UX &amp; UI design</span> to developing
+		<span class="badge text-bg-light">responsive</span> interfaces in
+		<span class="badge text-bg-light">React</span> or
+		<span class="badge text-bg-light">Angular</span> using
+		<span class="badge text-bg-light">Bootstrap</span> or pure
+		<span class="badge text-bg-light">CSS</span>, is
+		<strong>hidden behind the login screens and NDAs</strong>. More about it in
+		<a href="/Ivo_Gregurec-CV.pdf">my CV</a>.<br />
+	</p>
+	<h6>Some recent public projects:</h6>
+
+	<div class="row py-5">
+		<Hostelhop />
+		<Redocher />
+		<Vava />
+		<Spectrackular />
+		<Kyba />
+		<Old />
+	</div>
+</div>
 <Footer />
 
 <style global>
-	body {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
 	.content-wrapper {
 		flex: 1; /* Allows the content to grow and take up any available space */
 	}
